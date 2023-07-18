570610d01b7fa8c73a21b60f156e61f887735414
MONSTER <slawku1@gmail.com>
2023-07-18 22:53:09 +0200

    Angular course project init

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map, tap} from 'rxjs/operators';
+
+import {AuthService} from "../auth/auth.service";
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://ng-mealplanner-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+
+        return this.http
+          .get<Recipe[]>(
+          'https://ng-mealplanner-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+        ).pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
+  }
+}
